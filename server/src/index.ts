81de94895b40de3465df60b3f2beacf4aa56a7e5
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-11-23 22:59:16 +0530

    Initialize Express server with middleware

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+import Express  from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
